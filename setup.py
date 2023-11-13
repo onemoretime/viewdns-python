141842d557aec914b262732500674b1ae8ce60e2
One more time <fredjqn@gmail.com>
2023-11-13 17:49:17 +0100

    update: version and urls

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-import setuptools
+import setuptools  # type: ignore
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="viewdns-python",
-    version="0.0.1",
+    version="0.1.0-dev1",
     author="Michael Scribellito",
     author_email="mscribellito@gmail.com",
     description="Python module to interact with ViewDNS.info API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mscribellito/viewdns-python",
+    url="https://github.com/onemoretime/viewdns-python",
     project_urls={
-        "Bug Tracker": "https://github.com/mscribellito/viewdns-python/issues",
+        "Bug Tracker": "https://github.com/onemoretime/viewdns-python/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
